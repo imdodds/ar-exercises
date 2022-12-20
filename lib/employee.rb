ae79d665ff2c3cb3bd08aca38106bfa4222442da
Ian Dodds <ianmitchelldodds@gmail.com>
2022-12-20 17:51:08 +0000

    Validations for both models

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :store_id, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 30, less_than: 200 }
 end
